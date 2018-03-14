89e8c600a7c3468ac06d7924418e4bc6fce6e1bf
Miguel Palau <mpalauzarza@gmail.com>
2018-03-14 12:20:18 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: 'standard',
+  rules: {
+    'semi': 'off',
+    'comma-dangle': 'off'
+  },
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    jest: true,
+    node: true,
+  }
+}
